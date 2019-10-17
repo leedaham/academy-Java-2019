af635aa4a7c8766419dd978e61920fa594edd581
java <java@DESKTOP-064EL2N>
2019-10-17 11:25:28 +0900

    2019/10/17 ch01 project 등록

---
+++ ch01/src/p17/Hello.java
@@ -0,0 +1,14 @@
+package p17;
+
+/*
+ 작성자: 이다함 
+ 작성일: 2019/10/11
+ */
+
+public class Hello {
+	//프로그래 실행 진입점
+	public static void main(String[] args) {
+	//콘솔에 출력하는 실행문
+		System.out.println("Hello, welcome to the java world!");
+	} //end of main
+} 	  //end of class
---
+++ ch01/src/sub1/HelloJava.java
@@ -0,0 +1,17 @@
+package sub1;
+
+/*
+ * 날짜 : 2019.10.10
+ * 이름 : 이다함
+ * 내용 : 자바 Hello world 출력하기 
+ */
+
+public class HelloJava {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Hello Java!");
+		
+	}
+		
+}
